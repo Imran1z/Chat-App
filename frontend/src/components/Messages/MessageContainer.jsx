d38bb73484202cd503b839fdd7a2b79da24460d9
Imran1z <shaikhzimran12334@gmail.com>
2024-02-12 18:15:11 +0530

    ui completed

--- frontend/src/components/Messages/MessageContainer.jsx
+++ frontend/src/components/Messages/MessageContainer.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import MessageInput from './MessageInput'
 import Messages from './Messages'
+import { TiMessages } from "react-icons/ti";
+
 
 const MessageContainer = () => {
+  const noChatSelected =true;
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-        <>
+    {noChatSelected?(<NoChatSelected/>):(
+      <>
             {/* Header */}
 					<div className='bg-customGray px-4 py-2 mb-2'>
 						<span className='label-text'>To:</span>{" "}
@@ -14,8 +18,22 @@ const MessageContainer = () => {
                     <Messages/>
                     <MessageInput/>
         </>
+    )}
+        
     </div>
   )
 }
 
 export default MessageContainer
+
+const NoChatSelected = () => {
+	return (
+		<div className='flex items-center justify-center w-full h-full'>
+			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
+				<p>Welcome <span className='text-customGray font-bold'>Imran</span></p>
+				<p>Select a chat to start messaging</p>
+				<TiMessages className='text-3xl md:text-6xl text-center' />
+			</div>
+		</div>
+	);
+};
--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   return (
     <div className='border-r border-slate-500 p-4 flex flex-col '>
         <SearchInput/>
-        <div className='divider px-3'></div>
+        <div className='divider px-2'></div>
         <Conversations/>
         <LogoutButton/>
 
