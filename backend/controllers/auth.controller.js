5d20caabcdc6c9abc004ce50a5e265553b71a4f3
Imran1z <shaikhzimran12334@gmail.com>
2024-02-12 10:05:46 +0530

    creted the signup api and resolved some middleware issues

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,55 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-
-export const signup=(req, res)=>{
+export const signup=async (req, res)=>{
     try {
+        console.log(req.body)
+        const {fullName, username,password,confirmPassword, gender}= req.body;
+
+        if (password !== confirmPassword) {
+            return res.status(400).json({error:"Password don't match"})
+        }
+
+        const user =await User.findOne({username});
+
+        if (user) {
+            return res.status(400).json({error:"Username already exist"})
+        }
+
+        const hashedPassword = bcryptjs.hashSync(password,12)
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser =new User({
+            fullName,
+            username,
+            password:hashedPassword,
+            gender,
+            profilePic:gender=='male'?boyProfilePic:girlProfilePic,
+        });
+
+
+        if (newUser) {
+            await newUser.save();
+             const token =jwt.sign({id:newUser._id},process.env.JWT_SECRET,{expiresIn:'15d'});
+
+               const {password:hashedPassword,...user}=newUser._doc;
+
+               const expiryDate = new Date(Date.now()+(24 * 60 * 60 * 1000));
+
+
+               res.cookie('token',token ,{httpOnly:true,expires:expiryDate}).status(200).json({user});
+
+        }else{
+            res.status(400).json({ error: "Invalid user data" });
+        }
+
     } catch (error) {
-        
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });    
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ dotenv.config()
 const app = express()
 
 
+//middleware 
+app.use(express.json());
+app.use(cookieParser())
+app.use('/api/v1/auth',authRouter)
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{
     console.log("Connected to the database");
@@ -22,10 +26,5 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{
 })
 
 
-app.use('/api/v1/auth',authRouter)
 
 
-
-//middleware 
-app.use(express.json())
-app.use(cookieParser)
