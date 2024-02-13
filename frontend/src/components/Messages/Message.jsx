7e8fda5839c6d235a8e31948ea1cfcce985889db
Imran1z <shaikhzimran12334@gmail.com>
2024-02-13 17:21:14 +0530

    messages are rendering fine now

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import {useAuthContext} from '../../context/AuthContext'
+import useConversation from '../../zustand/useConversation';
+import { formatTime } from '../../utils/formatTime';
 
-const Message = () => {
+const Message = ({message}) => {
+  const{authUser}=useAuthContext();
+  const{selectedConversation}=useConversation();
+
+  const fromMe = message.senderId === authUser.user._id;
+
+  const chatClassName =fromMe? 'chat-end':'chat-start'
+
+  const profilePic =fromMe? authUser.user.profilePic :selectedConversation?.profilePic;
+
+  const bubbleColor= fromMe? 'bg-customGray':"";
+
+  const formatedTime = formatTime(message.createdAt);
+  
   return (
-    <div className='chat chat-end'>
+    <div className={`chat ${chatClassName}`}>
           <div className='chat-image avatar'>
             <div className='w-8 rounded-full'>
-              <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt='avatar'/>
+              <img src={profilePic} alt='avatar'/>
 
             </div>
           </div>
-          <div className='chat-bubble text-white bg-customGray'> hello imran</div>
-          <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>11:42</div>
+          <div className={`chat-bubble text-white ${bubbleColor} pb-2`}>{message.message}</div>
+          <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formatedTime}</div>
 
     </div>
   )
--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeletons from '../skeletons/MessageSkeletons';
+import { useAuthContext } from '../../context/AuthContext';
 
 const Messages = () => {
 	const{messages,loading} = useGetMessages();
-	console.log("messages",messages)
+	const lastMessageRef =useRef();
+
+
+	useEffect(()=>{
+		setTimeout(() => {
+			lastMessageRef.current?.scrollIntoView({behavior:"smooth"})
+		}, 100);
+	},[messages])
     return (
         		<div className='px-4 flex-1 overflow-auto'>
-        			<Message />
+				{
+					!loading && messages.length !== 0 && messages.map((message)=>(
+						<div key={message._id} ref={lastMessageRef}>
+						<Message
+							
+							message={message}
+						/>
+						</div>
+					))
+				}
+
+				{loading && [...Array(6)].map((_, idx) => <MessageSkeletons key={idx} />)}
+			   {!loading && messages.length === 0 && (
+				<p className='text-center'>Send a message to start the conversation</p>
+			)}
+        			
         			
         		</div>
   )
