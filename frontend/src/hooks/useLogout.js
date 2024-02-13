ffd4576a4b5fdaa44862aa116fa9b5f253f5efd3
Imran1z <shaikhzimran12334@gmail.com>
2024-02-13 13:12:40 +0530

    made the sigout hook completed the signout functionality

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../context/AuthContext';
+
+const useLogout = () => {
+  const [loading, setLoading] = useState(false);
+  const {setAuthUser}=useAuthContext()
+
+
+  const logout =async()=>{
+    setLoading(true)
+    try {
+        const res =await fetch('/api/v1/auth/signout',{
+            method: "POST",
+				headers: { "Content-Type": "application/json" },
+        })
+
+        const data =await res.json();
+        console.log(data);
+
+        if (data.error) {
+            throw new Error(data.error)
+            
+        }
+
+        localStorage.removeItem("chat-user");
+        setAuthUser(null);
+        
+    } catch (error) {
+        toast.error(error.message)
+        
+    }finally{
+        setLoading(false)
+    }
+
+  }
+
+  return {loading,logout};
+}
+
+export default useLogout
