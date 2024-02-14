e072da4706a87a53ad229919f4fede53c6ccadea
Imran1z <shaikhzimran12334@gmail.com>
2024-02-14 14:34:21 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
@@ -10,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname =path.resolve();
+
 // Middleware
 app.use(cors({
     origin: "http://localhost:5173" // Update with your React client origin
@@ -19,6 +22,13 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/message', messageRouter);
 app.use('/api/v1/users', userRouter);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get('*',(req, res)=>{
+    res.sendFile(path.join(__dirname,'frontend', 'dist', 'index.html'))
+});
+
+
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
